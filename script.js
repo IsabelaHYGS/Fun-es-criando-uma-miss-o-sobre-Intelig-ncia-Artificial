65cdc83a30716481528b279269a367b783fd4883
IsabelaHYGS <168226667+IsabelaHYGS@users.noreply.github.com>
2026-07-29 12:34:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,33 @@ const perguntas = [
     alternativas: ["Alternativa 1", "Alternativa 2"],
   },
 ];
+
+let atual = 0;
+let perguntaAtual;
+let historiaFinal = "";
+
+function mostraPergunta() {
+  perguntaAtual = perguntas[atual];
+  caixaPerguntas.textContent = perguntaAtual.enunciado;
+  mostraAlternativas();
+}
+function mostraAlternativas() {}
+
+mostraPergunta();
+
+function mostraAlternativas() {
+  for (const alternativa of perguntaAtual.alternativas) {
+    const botaoAlternativas = document.createElement("button");
+    botaoAlternativas.textContent = alternativa;
+    botao.addEventListener("click", () => respostaSelecionada(alternativa));
+    caixaAlternativas.appendChild(botaoAlternativas);
+  }
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+  const afirmacoes = opcaoSelecionada.afirmacoes;
+  historiaFinal += afirmacoes + " ";
+  atual++;
+  mostraPergunta();
+}
+ mostraPergunta();
