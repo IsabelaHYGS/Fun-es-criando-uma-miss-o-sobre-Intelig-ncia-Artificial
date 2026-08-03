662810b4da7d04bc1356e1c7f6edb65f5e85c8d1
IsabelaHYGS <168226667+IsabelaHYGS@users.noreply.github.com>
2026-08-03 12:30:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -60,7 +60,7 @@ function mostraAlternativas() {
   for (const alternativa of perguntaAtual.alternativas) {
     const botaoAlternativas = document.createElement("button");
     botaoAlternativas.textContent = alternativa;
-    botao.addEventListener("click", () => respostaSelecionada(alternativa));
+    botao.addEventListener("click", () => respostaSelecionada(opcao));
     caixaAlternativas.appendChild(botaoAlternativas);
   }
 }
