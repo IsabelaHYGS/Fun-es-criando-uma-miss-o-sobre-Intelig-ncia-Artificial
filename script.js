ed7f07885fbe561a245cb90ed8baad72880a7345
IsabelaHYGS <168226667+IsabelaHYGS@users.noreply.github.com>
2026-09-14 09:25:46 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,31 +4,6 @@ const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
-
-        texto: "Isso é maravilhoso!",
-        afirmacao: [
-          "Quis saber como usar IA no seu dia a dia.",
-          "Foi atrás de vídeos, artigos e mais informaçõe sobre como utilizar essa tecnologia.",
-        ],
-      },
-    ],
-  },
-];
-  {
-    enunciado:
-      "Com a descoberta desta tecnologia, chamada Inteligência Artificial (IA), uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia. No fim de uma aula ela pede que você escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude você toma?",
-    alternativas: [
-      {
-        texto: "Utiliza uma ferramenta de busca na internet que utilizza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
-        afirmacao: "Conseguiu utilizar a IA para buscar informações úteis."
-      },
-      {
-        texto: "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema.",
-        afirmacao: "Preferiu usar fontes tradicionais e reflexões próprias para o estudo."
-      }
-    ],
-  },
-  {
     enunciado:
       "Após a elaboração do trabalho, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
     alternativas: [
