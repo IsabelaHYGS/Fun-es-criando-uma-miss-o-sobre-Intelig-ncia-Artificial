0b73566c34cd9ef4e0512a1edddf7990330dfc29
IsabelaHYGS <168226667+IsabelaHYGS@users.noreply.github.com>
2026-09-14 09:26:42 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -35,3 +35,21 @@ const perguntas = [
     ],
   },
   {
+    enunciado:
+      "Após a elaboração do trabalho, a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+    alternativas: [
+      {
+        texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+        afirmacao: "Acredita no potencial da IA de impulsionar novas carreiras."
+      },
+      {
+        texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendo a importância de proteger os trabalhadores.",
+        afirmacao: "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética."
+      }
+    ],
+  },
+  {
+    enunciado:
+      "Ao final da discussão, você precisou criar uma imagem no computador que representasse o que pensa sobre IA. E agora?",
+    alternativas: [
+      {
